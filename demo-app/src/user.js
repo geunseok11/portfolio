443f6f917109271980842bbfb6c047a43ab9ce81
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-08-21 00:29:26 +0900

    ADD user.js

---
+++ demo-app/src/user.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export default function Users() {
+  return <h2>Users : 정근석</h2>;
+}
