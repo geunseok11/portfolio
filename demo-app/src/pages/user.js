f4814bb94aa01e102a18da01767cb683d26696ef
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2020-10-01 00:14:52 +0900

    SETTING next.js

--- demo-app/src/pages/user.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import logo from "./logo.svg";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-export default function Users() {
-  return <h2>Users : 정근석</h2>;
-}
